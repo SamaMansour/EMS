c76f8b177a44152dd6a5d3536b609c8453114980
Sama Mansour <samamansour306@gmail.com>
2022-04-04 03:03:48 +0300

    init commit

--- EMS project/Models/RequestVacation.cs
+++ EMS project/Models/RequestVacation.cs
@@ -1,16 +1,34 @@
-﻿namespace EMS_project.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS_project.Models
 {
     public class RequestVacation :EntityBase
     {
         public int EmployeeId { get; set; }
+        [ForeignKey ("EmployeeId")]
         public Employee? Employee { get; set; }
 
         public DateTime? RequestDate { get; set; }
 
         public int VacationTypeId { get; set; }
 
+        [ForeignKey("VacationTypeId")]
+        public VacationType VacationType { get; set; }
 
-        public VacationType MyProperty { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        public string? Comment { get; set; }
+
+        public bool Approved { get; set; }
+
+
+        public DateTime? DateApproved { get; set; }
     }
 }
