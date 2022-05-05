fc16e40273d20960119cf4f1eef4260d7f82db8e
Sama Mansour <samamansour306@gmail.com>
2022-05-05 18:28:36 +0300

    addMigrations

---
+++ EMS project/Migrations/20220505152734_addnewModelstoDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS_project.Migrations
+{
+    public partial class addnewModelstoDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
